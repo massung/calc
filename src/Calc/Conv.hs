fb8612a11a895d0b38ec0e5314a6230e662d8a1f
Jeffrey Massung <massung@gmail.com>
2022-06-26 18:07:15 -0500

    Work on SI units.

--- src/Calc/Conv.hs
+++ src/Calc/Conv.hs
@@ -45,34 +45,3 @@ conversions = fromRight [] $ V.toList . snd <$> decodeByName convCsv
 conversionScalar conv = Scalar (scale conv) (Just units)
   where
     units = multiplyUnits (to conv) (U.singleton (from conv) -1)
-
--- parseSIConversions csvBS = do
---   (_, rows) <- decodeByName csvBS :: Either String (Header, Vector SIUnits)
---   return $ L.concatMap convs $ V.toList rows
---   where
---     convs (SIUnits (_, u)) =
---       [ conv "z" u 1e-24,
---         conv "y" u 1e-21,
---         conv "a" u 1e-18,
---         conv "f" u 1e-15,
---         conv "p" u 1e-12,
---         conv "n" u 1e-9,
---         conv "u" u 1e-6,
---         conv "m" u 1e-3,
---         conv "c" u 1e-2,
---         conv "d" u 1e-1,
---         conv "da" u 10,
---         conv "h" u 1e2,
---         conv "k" u 1e3,
---         conv "M" u 1e6,
---         conv "G" u 1e9,
---         conv "T" u 1e12,
---         conv "P" u 1e15,
---         conv "E" u 1e18,
---         conv "Z" u 1e21,
---         conv "Y" u 1e24
---       ]
-
---     -- apply si prefix to base units with scale factor
---     conv prefix u scale =
---       Conv {from = Unit $ prefix ++ u, to = Unit u, scale = scale}
--- src/Calc/Units/Base.hs
+++ src/Calc/Units/Base.hs
@@ -7,9 +7,10 @@ import Data.ByteString.UTF8 as BS
 import Data.Csv
 import Data.Either
 import Data.FileEmbed
+import Data.List as L
+import Data.Map as M
 import Data.Maybe
 import Data.String
-import Data.Map as M
 import Data.Vector as V
 
 data Unit = Unit {name :: Maybe String, symbol :: String}
@@ -22,19 +23,45 @@ instance FromNamedRecord Unit where
   parseNamedRecord r = do
     name <- r .: "name"
     symbol <- r .: "symbol"
-    return $ Unit {name=Just name, symbol=symbol}
+    return $ Unit {name = Just name, symbol = symbol}
 
 instance IsString Unit where
   fromString s = fromMaybe unknown $ M.lookup s unitsMap
     where
-      unknown = Unit {name=Nothing, symbol=s}
+      unknown = Unit {name = Nothing, symbol = s}
+
+siPrefixes =
+  [ ("yocto", "y", 1e-24),
+    ("zepto", "z", 1e-21),
+    ("atto", "a", 1e-18),
+    ("femto", "f", 1e-15),
+    ("pico", "p", 1e-12),
+    ("nano", "n", 1e-9),
+    ("micro", "u", 1e-6),
+    ("milli", "m", 1e-3),
+    ("centi", "c", 1e-2),
+    ("deci", "d", 1e-1),
+    ("deka", "da", 10),
+    ("hecto", "h", 1e2),
+    ("kilo", "k", 1e3),
+    ("mega", "M", 1e6),
+    ("giga", "G", 1e9),
+    ("tera", "T", 1e12),
+    ("peta", "P", 1e15),
+    ("exa", "E", 1e18),
+    ("zetta", "Z", 1e21),
+    ("yotta", "Y", 1e24)
+  ]
 
 unitsCsv = $(embedStringFile "res/units.csv")
 
-parsedUnits = V.foldl foldUnits M.empty . snd <$> decodeByName unitsCsv
+unitsMap = fromRight M.empty $ V.foldl siUnits M.empty . snd <$> decodeByName unitsCsv
   where
-    foldUnits m u = insert (symbol u) u m
+    siUnits m u = L.foldl (insertSIUnits u) m siPrefixes
 
-unitsMap = fromRight M.empty parsedUnits
+    -- create new units with si prefixes
+    insertSIUnits u m (namePrefix, symbolPrefix, _) =
+      let s = namePrefix L.++ fromJust (name u)
+       in M.insert s (Unit {name=Just s, symbol=symbolPrefix L.++ symbol u}) m
 
 units = [u | (_, u) <- M.toList unitsMap]
