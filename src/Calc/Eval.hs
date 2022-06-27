fb4be96460914ff73fefc5505fc5da24d49856f9
Jeffrey Massung <massung@gmail.com>
2022-06-27 16:29:32 -0500

    Added eval converts back.

--- src/Calc/Eval.hs
+++ src/Calc/Eval.hs
@@ -6,15 +6,15 @@ import Calc.Scalar
 
 eval :: Expr -> Either String Expr
 eval (Term x) = Right $ Term x
-eval (Convert x to) = Right x -- evalConvert x to
+eval (Convert x to) = evalConvert x to
 eval (Unary _ f x) = evalUnary f x
 eval (Binary _ f x y) = evalBinary f x y
-eval (BinaryConvert _ f x y) = Right x -- evalBinaryConvert f x y
+eval (BinaryConvert _ f x y) = evalBinaryConvert f x y
 
--- evalConvert (Term x) to = convertScalar x to >>= eval . Term
--- evalConvert x to = do
---   x' <- eval x
---   evalConvert x' to
+evalConvert (Term x) to = convert x to >>= eval . Term
+evalConvert x to = do
+  x' <- eval x
+  evalConvert x' to
 
 evalUnary f (Term x) = eval $ Term (f x)
 evalUnary f x = eval x >>= evalUnary f
@@ -25,11 +25,11 @@ evalBinary f x y = do
   y' <- eval y
   evalBinary f x' y'
 
--- evalBinaryConvert f (Term x) (Term y@(Scalar _ Nothing)) = eval $ Term (f x y)
--- evalBinaryConvert f (Term x) (Term y@(Scalar _ (Just u))) = do
---   x' <- convertScalar x u
---   eval $ Term (f x' y)
--- evalBinaryConvert f x y = do
---   x' <- eval x
---   y' <- eval y
---   evalBinaryConvert f x' y'
+evalBinaryConvert f (Term x) (Term y@(Scalar _ Nothing)) = eval $ Term (f x y)
+evalBinaryConvert f (Term x) (Term y@(Scalar _ (Just u))) = do
+  x' <- convert x u
+  eval $ Term (f x' y)
+evalBinaryConvert f x y = do
+  x' <- eval x
+  y' <- eval y
+  evalBinaryConvert f x' y'
